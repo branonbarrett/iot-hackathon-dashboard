e773f04e01e0221bc5376bb95bdc86802ba337ae
Branon Barrett <branon.barrett@bluefletch.com>
2016-07-10 20:17:29 -0400

    added total count

--- app/src/reducers/reducer_events.js
+++ app/src/reducers/reducer_events.js
@@ -3,7 +3,7 @@ import { FETCH_EVENTS } from '../actions/index';
 export default function(state = {}, action) {
   switch (action.type) {
     case FETCH_EVENTS:
-      const count = action.payload.data._shards.total;
+      const count = action.payload.data.hits.total;
       const data = action.payload.data.hits.hits;
 
       return { count: count, data: data };
